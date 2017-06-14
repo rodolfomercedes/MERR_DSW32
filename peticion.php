a991cbf475e5c3590bf983900b3291ea7fcf973d
rodolfomercedes <rodolfo-mercedes@hotmail.com>
2017-06-14 10:02:45 -0500

    MERR_DSW32
    Rodolfo Antonio Mercedes Ramirez

---
+++ peticion.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+</head>
+
+<body>
+<p>&lt;?php</p>
+<p>echo '&lt;p id =&quot;nombre&quot;&gt; '.$_GET['nombre'}</p>
+<p>print_r($_REQUEST);</p>
+<p>?&gt;</p>
+</body>
+</html>
